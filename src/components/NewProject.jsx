6de73bac0fbc3551db55185e0c53ae7aa3940344
onkarsherkar <onkar.sherkar@gmail.com>
2026-01-14 22:49:41 +0530

    Input and NewProject components added

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,16 @@
+import Input from './Input'
+export default function NewProject(){
+    return <div>
+        <menu>
+            <li>
+                <button>Cancel</button>
+                <button>Save</button>
+            </li>
+        </menu>
+        <div>
+            <Input label="Title" />
+            <Input label="Description" textarea={true}/>
+            <Input label="Due Date" />
+        </div>
+    </div>
+}
