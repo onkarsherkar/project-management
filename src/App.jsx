b13e58589890398cd9b836dfbaf70c7528156e29
onkarsherkar <onkar.sherkar@gmail.com>
2026-01-17 21:37:56 +0530

    Handle project creation

--- src/App.jsx
+++ src/App.jsx
@@ -26,12 +26,11 @@ function App() {
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
 
@@ -43,7 +42,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
