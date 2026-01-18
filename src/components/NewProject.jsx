a707ee898d3f410d86f3799736bd735ff362aa05
onkarsherkar <onkar.sherkar@gmail.com>
2026-01-18 23:00:57 +0530

    tailwind styling

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 
 import Input from "./Input";
 import Modal from "./Modal";
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
   const title = useRef();
   const description = useRef();
@@ -31,14 +31,24 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption="Close">
-        <h2> Invalid Input</h2>
-        <p>Oops... forget to enter the value.</p>
-        <p>Please provide valid value to each input field</p>
+        <h2 className="text-xl font-bold text-stone-700 my-4">
+          {" "}
+          Invalid Input
+        </h2>
+        <p className="text-stone-600 mb-4">
+          Oops... forget to enter the value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please provide valid value to each input field
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
               Cancel
             </button>
             <button
